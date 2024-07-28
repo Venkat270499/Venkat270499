ea20580928f37cea673986a555b7d8e50790677d
Sai <venkat>
2024-07-28 11:18:45 +0530

    SpringPrac_Mvc updated proj

--- SpringPractice_MvcMiniProj/src/test/java/com/nt/SpringPracticeMvcMiniProjApplicationTests.java
+++ SpringPractice_MvcMiniProj/src/test/java/com/nt/SpringPracticeMvcMiniProjApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nt;
+/*package com.nt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class SpringPracticeMvcMiniProjApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
