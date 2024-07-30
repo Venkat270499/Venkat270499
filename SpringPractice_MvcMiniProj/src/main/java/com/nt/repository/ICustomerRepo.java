4a12951cef107d4698431d001499319097a4d753
EC2 Default User <ec2-user@ip-172-31-12-74.ap-southeast-2.compute.internal>
2024-07-30 18:25:30 +0000

    docker file updated

--- SpringPractice_MvcMiniProj/src/main/java/com/nt/repository/ICustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.model.CustomerClass;
-
-public interface ICustomerRepo extends JpaRepository<CustomerClass, Integer> {
-
-}
